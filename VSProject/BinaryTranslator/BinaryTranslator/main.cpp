8bfdd45729878c45988599dac03088831a213e3c
AdamCerutti <46689357+AdamCerutti@users.noreply.github.com>
2022-04-19 11:09:19 -0400

    Final main

--- VSProject/BinaryTranslator/BinaryTranslator/main.cpp
+++ VSProject/BinaryTranslator/BinaryTranslator/main.cpp
@@ -8,28 +8,23 @@ using namespace std;
 int main() {
 
 	BinaryTranslator trans;
+	string txt, bin, hx;
 
-	cout << "Binary to English: Enter Binary" << endl;
-	string bin;
+	cout << "Binary to Text: Enter Binary" << endl;
 	getline(cin, bin);
 	cout << trans.BinaryToText(bin) << endl;
 
-	cout << "English to Binary: Enter English" << endl;
-	string eng;
-	getline(cin, eng);
-	cout << trans.TextToBinary(eng) << endl;
+	cout << "Text to Binary: Enter Text" << endl;
+	getline(cin, txt);
+	cout << trans.TextToBinary(txt) << endl;
 
 	cout << "Text to Hexadecimal: Enter Text" << endl;
-	string txt;
 	getline(cin, txt);
 	cout << trans.TextToHex(txt) << endl;
 
 	cout << "Hexadecimal to Text: Enter Hex" << endl;
-	string hx;
 	getline(cin, hx);
 	cout << trans.HexToText(txt) << endl;
-
-
 
 	return 0;
 }
